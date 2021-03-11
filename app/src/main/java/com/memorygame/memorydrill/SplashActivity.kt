cff3d50e3437728ea78596339251ad6c11c1f0c5
Pallavi <pallavijoshi4@gmail.com>
2021-03-11 09:41:52 +0100

    convert SplashActivity to kotlin

--- app/src/main/java/com/memorygame/memorydrill/SplashActivity.kt
+++ app/src/main/java/com/memorygame/memorydrill/SplashActivity.kt
@@ -1,48 +1,39 @@
-package com.memorygame.memorydrill;
+package com.memorygame.memorydrill
 
-import android.content.Intent;
-import android.graphics.Typeface;
-import android.os.Bundle;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 /**
  * Created by aspire on 04-07-2016.
  */
-public class SplashActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(com.memorygame.memorydrill.R.layout.splash);
-
-        TextView tvMasterMind = (TextView) findViewById(com.memorygame.memorydrill.R.id.tvMasterMind);
-
-        Typeface face = Typeface.createFromAsset(getAssets(), "fonts/papyrus.ttf");
-        tvMasterMind.setTypeface(face);
-
-        Thread timer = new Thread() {
-            public void run() {
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash)
+        val tvMasterMind = findViewById<View>(R.id.tvMasterMind) as TextView
+        val face = Typeface.createFromAsset(assets, "fonts/papyrus.ttf")
+        tvMasterMind.typeface = face
+        val timer: Thread = object : Thread() {
+            override fun run() {
                 try {
-                    sleep(4000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    sleep(4000)
+                } catch (e: InterruptedException) {
+                    e.printStackTrace()
                 } finally {
-
-                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
-                    startActivity(intent);
+                    val intent = Intent(this@SplashActivity, MainActivity::class.java)
+                    startActivity(intent)
                 }
             }
-
-        };
-        timer.start();
+        }
+        timer.start()
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        finish();
-
+    override fun onPause() {
+        super.onPause()
+        finish()
     }
 }
